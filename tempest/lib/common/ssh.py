c36a89269701f864f2640051ab518a270606cc04
Mikhail Samoylov <mikhailsamoiloff@gmail.com>
2022-04-16 10:48:39 +0300

    Increase sleep time for ssh

--- tempest/lib/common/ssh.py
+++ tempest/lib/common/ssh.py
@@ -106,7 +106,7 @@ class Client(object):
                 host=self.host, port=self.port, username=self.username)
         self._proxy_conn = None
 
-    def _get_ssh_connection(self, sleep=2, backoff=1.5):
+    def _get_ssh_connection(self, sleep=5, backoff=1):
         """Returns an ssh connection to the specified host."""
         bsleep = sleep
         ssh = paramiko.SSHClient()
